110eb72d39d6e2d038236dffbbce55640dc98077
Mario <marinaldo12@hotmail.com>
2025-08-28 15:58:44 -0300

    docs de como funciona a as.view() atualizado

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('singin/', Singin.as_view()),
-    path('singup/', Singup.as_view())
+    path('singup/', Singup.as_view()) 
 ]
+"""é o tipo de requisição HTTP (GET, POST, PUT, DELETE, etc.) que determina qual método dentro da sua classe será executado de forma automatica, por isso não precisamos passar diretamente a função especifica, fora que a as.view() "adapta" a classe para ser lida como uma função(a forma padrão de recebimento), é uma forma comum e moderna"""
