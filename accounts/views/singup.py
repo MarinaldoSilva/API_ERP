c7963296d46e5984beb474e38d8b1bcb71fab8a1
Mario <marinaldo12@hotmail.com>
2025-08-28 16:07:00 -0300

    tipagem do request, adicição de captura de excessões, adição de verbos http para retorno mais preciso e amigavel ao usuário caso erros na execução

--- accounts/views/singup.py
+++ accounts/views/singup.py
@@ -1,19 +1,31 @@
-from accounts.views.base import Base
+from rest_framework.views import APIView
 from accounts.auth import Authentication
 from accounts.serializers import UserSerializer
-from rest_framework.response import Response
 
-class Singup(Base):
-    def post(self, request):
+from rest_framework.response import Response
+from rest_framework.exceptions import APIException
+from rest_framework import status
+from django.http import HttpRequest
+
+#cadastro
+class Singup(APIView):
+    def post(self, request: HttpRequest): #variavel : tipo da variavel
         name = request.data.get('name')
         email = request.data.get('email')
         password = request.data.get('password')
-
         authentication = Authentication()
-        user = authentication.signup(name=name, email=email,password=password)
 
-        serializer = UserSerializer(user)
+        try:
+            user = authentication.signup(name=name,email=email,password=password)
 
-        return Response({
-            'user':serializer.data
-        })
+            serializer = UserSerializer(user)
+            return Response(
+                {'user': serializer.data},
+                status=status.HTTP_201_CREATED
+            )
+        
+        except APIException as e:
+            return Response(
+                {'detail': str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
