126b3dca9fc195f6a9db4e4326b19a9b33fc33f6
Mario <marinaldo12@hotmail.com>
2025-08-28 16:05:10 -0300

    importação do django.http para retorna mensagens de erros mais precisas, tipando a request para facilitar o uso de metodos nativos com a notação ponto, conversão dos tokens para srt(), com a eliminação do APIView da classe Base, tivemos que instanciar um objeto para ter acesso ao get_enterprise_user() e importamos de forma direta o APIView para usar nas nossas urls

--- accounts/views/singin.py
+++ accounts/views/singin.py
@@ -1,19 +1,34 @@
-from accounts.views.base import Base
 from accounts.auth import Authentication
 from accounts.serializers import UserSerializer
-
+from accounts.views.base import Base
+from django.http import HttpRequest
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 
-class Singin(Base):
-    def post(self, request):
+class Singin(APIView):
+    def post(self, request: HttpRequest):
         email = request.data.get('email')
         password = request.data.get('password')
 
-        authentication = Authentication()
-        user = authentication.singin(self, email=email, password=password)
+        if not all([email, password]):
+            return Response(
+                {"detail":"Email e senha são obrigatórios"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
-        enterprise = self.get_enterprise_user(user.id)
+        authentication = Authentication()
+        user = authentication.singin(email=email, password=password)
+
+        if not user:
+            return Response(
+                {"detail":"Credenciais de usário invalidas"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        base = Base()
+        enterprise = base.get_enterprise_user(user.id)
         
         serializer = UserSerializer(user)
 
@@ -21,7 +36,7 @@ class Singin(Base):
 
         return Response({
             "user": serializer.data,
-            "enterpize": enterprise,
-            "refresh": RefreshToken ,
-            "acess_token": token.access_token
+            "enterprise": enterprise,
+            "refresh": str(token) ,
+            "access_token": str(token.access_token)
         })
